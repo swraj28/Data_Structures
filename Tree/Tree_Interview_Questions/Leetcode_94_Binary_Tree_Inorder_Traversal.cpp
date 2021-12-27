4f45be25043915f002426b9b23a7ca521f693247
swraj28 <swarajkant@gmail.com>
2021-12-27 14:55:38 +0530

    27-12-2021->Tree

--- Tree/Tree_Interview_Questions/Leetcode_94_Binary_Tree_Inorder_Traversal.cpp
+++ Tree/Tree_Interview_Questions/Leetcode_94_Binary_Tree_Inorder_Traversal.cpp
@@ -29,28 +29,91 @@ public:
 
 	vector<int> res;
 
+	// 1. Recursion:-
+
+	// T.c:- O(nodes) and S.c:- Space Occupied while building recursion Stack--> In worst case O(node)--> skew Tree(Left Skew)
+	void inOrder_rec(TreeNode*root) {
+		if (root == NULL) {
+			return;
+		}
+
+		inOrder_rec(root->left);
+		res.pb(root->val);
+		inOrder_rec(root->right);
+	}
+
+	// 2. Iterative Using an Explicit Stack:-
+
+	/*
+	   T.c:- O(nodes)
+	   S.c:- O(nodes)--> If we consider a left skew tree
+	*/
+
 	void in_order_itr(TreeNode*root) {
 
 		stack<TreeNode*>s;
 		TreeNode*curr = root;
 
-		while (!s.empty() or curr != nullptr) {
+		while ((!s.empty()) or (curr != nullptr)) { // why have we used current in while loop? (Make a dry run of right skew tree and it will be clear)
 
 			while (curr != NULL) {
 				s.push(curr);
 				curr = curr->left;
 			}
+
 			curr = s.top();
 			res.pb(curr->val);
+
 			s.pop();
 
 			curr = curr->right;
 		}
 	}
 
+	// 3. Morris Traversal For Inorder Traversal:-
+
+	/*
+	    T.c:- O(nodes)
+	    S.c:- O(1)
+
+	*/
+
+	void inOrder_morris(TreeNode* root) {
+		if (root == nullptr) {
+			return;
+		}
+
+		TreeNode* curr = root;
+
+		while (curr != nullptr) {
+
+			if (curr->left == nullptr) {
+				res.pb(curr->val);
+				curr = curr->right;
+			} else {
+
+				TreeNode* succ = curr->left;
+
+				while ((succ->right != nullptr) and (succ->right != curr)) {
+					succ = succ->right;
+				}
+
+				if (succ->right == nullptr) {
+					succ->right = curr;
+					curr = curr->left;
+				} else {
+					res.pb(curr->val);
+					curr = curr->right;
+				}
+			}
+		}
+	}
+
 	vector<int> inorderTraversal(TreeNode* root) {
 
-		in_order_itr(root);
+		// in_order_itr(root);
+
+		inOrder_morris(root);
 
 		return res;
 	}

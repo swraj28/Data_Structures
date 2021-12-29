f64559f424c6b0f03f933881a60bc6103f76f80e
swraj28 <swarajkant@gmail.com>
2021-12-29 15:08:40 +0530

    29-12-2021

--- Tree/Tree_Interview_Questions/Leetcode_968_Binary_Tree_Camera.cpp
+++ Tree/Tree_Interview_Questions/Leetcode_968_Binary_Tree_Camera.cpp
@@ -23,45 +23,71 @@ struct TreeNode {
 	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+/*
+    Some Important observations:-
+
+    1. If the tree has Single node then we will place the camera on the root node otherwise let recursion decide whether we require
+       to place the Camera on the root node or not.
+
+    2. It is not ideal to place the Camera on the leaf node. As the Camera Covers its child as well its Parent, but the leaf node doesn't
+       have any child node. Leaf Nodes will always be covered by some of its Parents.
+
+    3. We will Perform Post Order Traversal.
+
+    4. If some node returns Covered (i.e., 1) then that node is not under the cover of some Camera. That Node will ask its Parent to place a Camera.
+       If Parent is there (i.e., root) then we will place the camera on that particular node itself.
+
+*/
+
 class Solution {
 public:
 
+#define Need_Camera -1  // If a node returns -1 then a Camera must be placed at its parent node.
+#define Has_Camera 0   // If a camera is placed on a particular node then it will return 0.
+#define Covered 1      // If the parent of the node has camera then its returns 1.  
+
+	int ans = 0;
+
 	int dfs(TreeNode* root) {
 		if (root == nullptr) {
-			return 0;
+			return Covered;
 		}
 
-		int l = 0, r = 0;
-
-		// If i decided to place the camera on the root node
-
-		if (root->left) {
-			l += dfs(root->left->left);
-			l += dfs(root->left->right);
+		if ((root->left == nullptr) and (root->right == nullptr)) {
+			return Need_Camera;
 		}
 
-		if (root->right) {
-			r += dfs(root->right->left);
-			r += dfs(root->right->right);
+		int l = dfs(root->left);
+		int r = dfs(root->right);
+
+		if (l == Need_Camera or r == Need_Camera) {
+			ans++;
+			return Has_Camera;
 		}
 
-		int sm = 1 + l + r;
+		if (l == Has_Camera or r == Has_Camera) {
+			return Covered;
+		}
 
-		return sm;
+		return Need_Camera;
 	}
+
 
 	int minCameraCover(TreeNode* root) {
 		if (root == nullptr) {
 			return 0;
 		}
 
-		if (root->left == nullptr and root->right == nullptr) {
+		if ((root->left == nullptr) and (root->right == nullptr)) {
 			return 1;
 		}
 
-		int val1 = dfs(root);
-		int val2 = dfs(root->left) + dfs(root->right);
+		int x = dfs(root);
 
-		return min({val2, val1});
+		if (x == -1) { // Needed to place the caemera on the root.
+			ans++;
+		}
+
+		return ans;
 	}
 };
--- Tree/Tree_Interview_Questions/Leetcode_98_Validate_BST.cpp
+++ Tree/Tree_Interview_Questions/Leetcode_98_Validate_BST.cpp
@@ -23,9 +23,57 @@ struct TreeNode {
 	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+/*
+     Important Points:-
+
+     1. Inorder Traversal Of BST gives sorted array. The Brute force solution is that find the inorder traversal of the BT and if it is sorted
+        then it will be BST. (Extra Space :- O(nodes))
+
+     2. We can even solve this without any Extra Space.
+        --> If we would obtained the inorder Traversal then we would have use used Two Pointer Technique to check whether the array is sorted or not.
+        --> But we cannot store the inorder Traversal, we will use similar Two Pointer Technique to validate the BST.
+
+        ---> How?
+
+        ---> we will use a prev pointer that points to the prev node and the curr pointer is pointed by the root and we can make make comparisons like
+             this.
+
+*/
+
 class Solution {
 public:
+
+	TreeNode* prev = nullptr;
+
+	bool dfs(TreeNode* root) {
+		if (root == nullptr) {
+			return true;
+		}
+
+		bool left_bst = dfs(root->left);
+
+		if (left_bst == false) {
+			return false;
+		}
+
+		if ((prev != nullptr) and (prev->val) >= (root->val)) {
+			return false;
+		}
+
+		prev = root;
+
+		bool right_bst = dfs(root->right);
+
+		if (right_bst == false) {
+			return false;
+		}
+
+		return true;
+	}
+
 	bool isValidBST(TreeNode* root) {
 
+
+		return dfs(root);
 	}
 };
